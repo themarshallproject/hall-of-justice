c30f80dafe38a001a0995e1c92eddc35725391f3
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-03-13 16:26:37 -0400

    Tweaking models after some data tests

--- cjdata/models.py
+++ cjdata/models.py
@@ -4,45 +4,52 @@ from django.contrib.postgres.fields import ArrayField
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    parent = models.ForeignKey("self", related_name="children")
+    parent = models.ForeignKey("self", related_name="children", null=True, blank=True)
 
     class Meta:
         verbose_name = "Category"
-        verbose_name_plural = "Categoryies"
+        verbose_name_plural = "Categories"
+        ordering = ['parent__name', 'name']
 
-    def __str__(self):
+    @property
+    def pathname(self):
         if self.parent:
             return "{parent_name}/{o.name}".format(o=self, parent_name=str(self.parent))
         else:
             return "{o.name}".format(o=self)
 
+    def __str__(self):
+        return self.pathname
+
 
 class Dataset(models.Model):
-    state = models.CharField(max_length=32)
-    sublocation = models.CharField(max_length=128)
+    state = models.CharField(max_length=40)
+    sublocation = models.CharField(blank=True, max_length=150)
     categories = models.ManyToManyField("Category")
-    title = models.CharField(max_length=150)
-    description = models.TextField(blank=True, null=True)
-    formats = ArrayField(models.CharField(max_length=32))
-    url = models.URLField(blank=True, null=True)
-    sectors = ArrayField(models.CharField(max_length=32),
-                         help_text="Sectors such as 'Private' or 'Government' or 'Non-Profit'")
+    title = models.TextField()
+    description = models.TextField(blank=True)
+    formats = ArrayField(models.CharField(max_length=40), blank=True, default=[],
+                         help_text="Enter formats, separated by commas")
+    url = models.URLField(blank=True, null=True, max_length=500)
+    sectors = ArrayField(models.CharField(max_length=40), blank=True, default=[],
+                         help_text="Sectors such as 'Private' or 'Government' or 'Non-Profit', separated by commas")
     group_name = models.CharField(help_text="Name of group administering dataset", max_length=150)
-    location = models.CharField(blank=True, null=True, max_length=64)
-    associated_legislation = models.CharField(blank=True, null=True, max_length=150)
+    location = models.TextField(blank=True)
+    associated_legislation = models.TextField(blank=True)
     internet_available = models.NullBooleanField()
     population_data = models.NullBooleanField()
     mappable = models.NullBooleanField()
     updated = models.NullBooleanField()
-    frequency = models.CharField(blank=True, null=True, max_length=50)
-    data_range = models.CharField(blank=True, null=True, max_length=100)
-    associated_grant = models.CharField(blank=True, null=True, max_length=100)
-    tags = ArrayField(models.CharField(max_length=50))
-    access_type = models.CharField(blank=True, null=True, max_length=50)
+    frequency = models.CharField(blank=True, max_length=50)
+    data_range = models.CharField(blank=True, max_length=50)
+    associated_grant = models.TextField(blank=True)
+    tags = ArrayField(models.CharField(max_length=50), blank=True, default=[],
+                      help_text="Tags, separated by commas")
+    access_type = models.CharField(blank=True, max_length=50)
 
     class Meta:
         verbose_name = "Dataset"
         verbose_name_plural = "Datasets"
 
     def __str__(self):
-        return "{o.state} ({o.private_or_government}): {o.title}".format(o=self)
+        return "{o.state} ({sectors}): {o.title}".format(o=self, sectors=",".join(self.sectors))
