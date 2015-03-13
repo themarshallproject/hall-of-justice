d3f8482ad1783f824fc5798902ddbcada50a42e6
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-03-13 16:26:20 -0400

    Futher config of admin

--- cjdata/admin.py
+++ cjdata/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from cjdata.models import (Category, Dataset)
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Category
+    '''
+    list_display = ('name', 'parent')
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Dataset
+    '''
+    list_display = ('title', 'state', 'tags', 'url')
+    list_filter = ('state',)
+    search_fields = ['title', 'description', 'tags']
+    filter_horizontal = ('categories',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Dataset, DatasetAdmin)
---
+++ cjdata/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CJDataAppConfig(AppConfig):
+    name = 'cjdata'
+    verbose_name = "Criminal Justice Data"
