907d5941198861bef1c3dde84a935b89c673eb9e
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-03-24 17:57:45 -0400

    Add some boosting and other search customizations that could be handy.

--- cjdata/search/indexes.py
+++ cjdata/search/indexes.py
@@ -1,19 +1,21 @@
 from haystack import indexes
-from elasticstack.fields import CharField
 from cjdata.models import Dataset
 import datetime
 
 
 class DatasetIndex(indexes.ModelSearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    states = indexes.FacetMultiValueField()
-    sectors = indexes.FacetMultiValueField(model_attr='sectors')
-    tags = indexes.FacetMultiValueField(model_attr='tags')
+    title = indexes.CharField(model_attr='title', boost=1.25)
+    description = indexes.CharField(model_attr='description', boost=1.125)
     group_name = indexes.CharField(model_attr='group_name', faceted=True)
+    states = indexes.FacetMultiValueField(model_attr='states')
+    sectors = indexes.FacetMultiValueField(model_attr='sectors')
+    tags = indexes.MultiValueField(model_attr='tags', boost=0.8)
+    formats = indexes.MultiValueField(model_attr='formats', boost=0.6)
 
     class Meta:
         model = Dataset
-        fields = ['title', 'description', 'group_name']
+        excludes = ['uuid', 'created_at', 'updated_at', 'formats']
 
     def prepare_states(self, obj):
         states_list = list(obj.states_expanded())
