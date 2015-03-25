803c8b1c6761204d0f7eb495c6bb039194b6a703
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-03-25 18:32:16 -0400

    Add WIP autocomplete view (something is wrong with index at moment).

--- cjdata/search/views.py
+++ cjdata/search/views.py
@@ -1,4 +1,7 @@
 from haystack.views import FacetedSearchView
+from haystack.query import SearchQuerySet
+from django.views.generic import View
+from django.http import JsonResponse
 
 
 class BetterFacetedSearchView(FacetedSearchView):
@@ -11,3 +14,17 @@ class BetterFacetedSearchView(FacetedSearchView):
             extra['selected_facets'] = dict(selected_facets)
 
         return extra
+
+
+class AutocompleteView(View):
+    """provides autocompletion JSON endpoint"""
+
+    def get(self, request):
+        query = request.GET.get('q', '')
+        sqs = SearchQuerySet().using('autocomplete').filter(content=query)
+
+        suggestions = set([s.path for s in sqs])
+
+        response = JsonResponse({'results': list(suggestions)})
+
+        return response
--- cjdata/urls.py
+++ cjdata/urls.py
@@ -3,7 +3,7 @@ from haystack.forms import FacetedSearchForm
 from haystack.views import search_view_factory
 
 from cjdata.search.query import sqs
-from cjdata.search.views import BetterFacetedSearchView
+from cjdata.search.views import (BetterFacetedSearchView, AutocompleteView,)
 from cjdata.views import DatasetDetailView
 
 urlpatterns = patterns(
@@ -12,4 +12,5 @@ urlpatterns = patterns(
     url(r'^$', search_view_factory(view_class=BetterFacetedSearchView,
                                    form_class=FacetedSearchForm,
                                    searchqueryset=sqs), name='haystack_search'),
+    url(r'^autocomplete/$', AutocompleteView.as_view(), name='haystack_autocomplete'),
 )
