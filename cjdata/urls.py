cb196a94379967c833a2064ab1d569ac168a5431
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-03-30 15:00:58 -0400

    Create category pages.

--- cjdata/urls.py
+++ cjdata/urls.py
@@ -4,11 +4,12 @@ from haystack.views import search_view_factory
 
 from cjdata.search.query import sqs
 from cjdata.search.views import (BetterFacetedSearchView, AutocompleteView,)
-from cjdata.views import DatasetDetailView
+from cjdata.views import (DatasetDetailView, CategoryDatasetsView)
 
 urlpatterns = patterns(
     '',
     url(r'^data/(?P<uuid>[a-f0-9-]{32,36})/$', DatasetDetailView.as_view(), name='dataset-detail'),
+    url(r'^category/(?P<path>[\w/-]+)/$', CategoryDatasetsView.as_view(), name='dataset-list'),
     url(r'^$', search_view_factory(view_class=BetterFacetedSearchView,
                                    form_class=FacetedSearchForm,
                                    searchqueryset=sqs), name='haystack_search'),
--- cjdata/views.py
+++ cjdata/views.py
@@ -1,5 +1,6 @@
-from django.views.generic.detail import DetailView
-from cjdata.models import Dataset
+from django.views.generic import DetailView, ListView
+from django.shortcuts import get_object_or_404
+from cjdata.models import Dataset, Category
 from cjdata.search.query import sqs
 
 
@@ -11,4 +12,19 @@ class DatasetDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(DatasetDetailView, self).get_context_data(**kwargs)
         context['more_like_this'] = sqs.more_like_this(self.object)[:100]
+        return context
+
+
+class CategoryDatasetsView(ListView):
+    model = Dataset
+    paginate_by = 50
+
+    def get_queryset(self):
+        path_arg = self.kwargs.get('path', None)
+        self.category = get_object_or_404(Category, path=path_arg.replace('-', ' '))
+        return Dataset.objects.filter(categories__path__iexact=self.category.path)
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDatasetsView, self).get_context_data(**kwargs)
+        context['category'] = self.category
         return context
