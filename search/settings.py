503222f13bfa1bd8a82e0ad3daf2b695e32b3c8c
Daniel Cloud <daniel@danielcloud.org>
2015-06-22 14:50:50 -0400

    Make search backend use the query_string search model from Haystack, but be looser about boolean keywords. Make a note in search/settings.py that multi-word synonyms should map to single word phrases.

--- search/settings.py
+++ search/settings.py
@@ -2,27 +2,29 @@ from cjdata.models import STATE_NATL_LOOKUP
 
 STATES_SYNONYMS = ["{}=>{}".format(v.lower(), k.lower()) for k, v in STATE_NATL_LOOKUP.items()]
 
+'''
+In order to make multi-word synonyms compatible with the synonym filter,
+we must map multi-word phrases to tokens without spaces, like "shu" or "stopandfrisk".
+'''
+
 cj_synonyms = [
     "homicide,murder,kill",
-    "close management=>close management,shu",
-    "solitary housing unit=>solitary housing unit,shu",
-    "solitary confinement=>solitary confinement,shu",
-    "shu,solitary=>shu",
-    "stop frisk,terry stop,pedestrian stop,stops,stop search,stop question frisk",
-    "death penalty,capital punishment",
-    "deconfliction,information sharing",
+    "close management,shu,solitary,solitary housing unit,solitary confinement=>shu",
+    "stop frisk,terry stop,pedestrian stop,stops,stop search,stop question frisk=>stopandfrisk",
+    "death penalty,capital punishment=>deathpenalty",
+    "deconfliction,information sharing=>deconfliction",
     "arrests,bookings",
-    "public legal services,indigent defense,public defenders",
-    "calls service,calls assistance,CAD,911 call,CFS,dispatch",
-    "domestic violence,intimate partner violence,domestic abuse,dating violence",
-    "use force,officer-involved shooting,death custody,arrest-related death",
+    "public legal services,indigent defense,public defenders=>publicdefenders",
+    "calls service,calls assistance,CAD,911 call,CFS,dispatch=>911call",
+    "domestic violence,intimate partner violence,domestic abuse,dating violence=>domesticviolence",
+    "use force,officer-involved shooting,death custody,arrest-related death=>useofforce",
     "larceny,theft",
     "prison,jail",
     "exoneration,pardon,dismissal",
-    "restitution,victim compensation,compensation",
-    "juvenile delinquent,juvenile,delinquent",
+    "restitution,victim compensation,compensation=>restitution",
+    # "juvenile delinquent,juvenile,delinquent=>juvenile",
     "parole,probation",
-    "part 1 crime,index crime",
+    "part 1 crime,part one crime,index crime=>part1crime",
     "k9,canine=>dog"
 ]
 cj_synonyms.extend(STATES_SYNONYMS)
