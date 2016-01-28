9d0edf22a239e363b8d25df72aa70377252b8ce4
crdunwel <clayton.dunwell@gmail.com>
2016-01-27 19:43:02 -0500

    add filters to category/location listviews like search #43

--- cjdata/templatetags/cjdata_filters.py
+++ cjdata/templatetags/cjdata_filters.py
@@ -3,6 +3,22 @@ import requests
 import re
 
 register = template.Library()
+url_page_re = re.compile('(\&)?page=\d+')
+
+
+def doctor_get_params_func(context, param):
+    full_path = context.request.get_full_path()
+    if context.request.GET:
+        return full_path + '&' + param
+    else:
+        return full_path + '?' + param
+
+
+@register.simple_tag(name='doctor_filter_link', takes_context=True)
+def doctor_filter_link(context, param):
+    full_path = doctor_get_params_func(context, param)
+    full_path = re.sub(url_page_re, '', full_path)
+    return full_path
 
 
 @register.filter(name='format_data_range')
@@ -20,7 +36,6 @@ def sort_by_state(queryset):
     return queryset.order_by('states')
 
 
-url_page_re = re.compile('page=\d+')
 @register.simple_tag(name='pagination_url_doctor', takes_context=True)
 def pagination_url_doctor(context, page):
     full_path = context.request.get_full_path()
@@ -28,9 +43,4 @@ def pagination_url_doctor(context, page):
     if re.search(url_page_re, full_path):
         return re.sub(url_page_re, page, full_path)
     else:
-        if context.request.GET:
-            return full_path + '&' + page
-        else:
-            return full_path + '?' + page
-
-
+        return doctor_get_params_func(context, page)
--- cjdata/views.py
+++ cjdata/views.py
@@ -4,6 +4,8 @@ from cjdata.models import Dataset, Category, STATE_NATL_LOOKUP
 from search.query import sqs
 from common.views import CSVExportMixin
 from django.db.models import Q
+import itertools
+from collections import Counter
 
 
 class IndexView(TemplateView):
@@ -21,44 +23,78 @@ class DatasetDetailView(DetailView):
         return context
 
 
-class CategoryDatasetsView(ListView):
+class DatasetsView(ListView):
+
+    def complete_query(self, query):
+        # filter query based on selected_facets GET parameters
+        if self.request.GET.getlist('selected_facets', False):
+            self.selected_facets = dict([(k.replace('_exact', ''), v) for k, v in (item.split(':') for item in self.request.GET.getlist('selected_facets'))])
+        else:
+            self.selected_facets = {}
+
+        for k, v in self.selected_facets.items():
+            if k in ['states', 'sectors']:
+                query = query & Q(**{k+'__contained_by': [v]})
+            else:
+                query = query & Q(**{k: v})
+
+        return query
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # counter number of each states, sectors, group_name, and access_type
+        obj_list = self.object_list
+        context['states'] = Counter([val for sublist in [x.states for x in obj_list] for val in sublist]).items()
+        context['sectors'] = Counter([val for sublist in [x.sectors for x in obj_list] for val in sublist]).items()
+        context['group_name'] = Counter([x.group_name for x in obj_list]).items()
+        context['access_type'] = Counter([x.access_type for x in obj_list]).items()
+        context['selected_facets'] = self.selected_facets
+
+        return context
+
+
+class CategoryDatasetsView(DatasetsView):
     model = Dataset
     paginate_by = 50
 
     def get_queryset(self):
+
         cat_slug = self.kwargs.get('category', None)
         subcat_slug = self.kwargs.get('subcategory', None)
         if cat_slug != 'none':
             self.category = get_object_or_404(Category, slug=cat_slug, parent__isnull=True)
             if subcat_slug:
                 self.category = get_object_or_404(Category, slug=subcat_slug, parent=self.category)
-                return self.category.dataset_set.all()
+                query = Q(categories=self.category)
             else:
                 query = (Q(categories=self.category) | Q(categories__parent=self.category))
         else:
             query = Q(categories__isnull=True)
 
-        return Dataset.objects.filter(query)
+        return Dataset.objects.filter(super().complete_query(query))
 
     def get_context_data(self, **kwargs):
-        context = super(CategoryDatasetsView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['category'] = getattr(self, 'category', None)
         return context
 
 
-class StateDatasetsView(ListView):
+class StateDatasetsView(DatasetsView):
     model = Dataset
     paginate_by = 50
 
     def get_queryset(self):
         self.state_abbr = self.kwargs.get('location', '').upper()
-        return Dataset.objects.filter(states__contains=[self.state_abbr])
+        query = Q(states__contains=[self.state_abbr])
+        return Dataset.objects.filter(super().complete_query(query))
 
     def get_context_data(self, **kwargs):
-        context = super(StateDatasetsView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['location_abbr'] = self.state_abbr
         context['location'] = STATE_NATL_LOOKUP.get(self.state_abbr, None)
         return context
+
 
 class StatesExportView(CSVExportMixin, StateDatasetsView):
     paginate_by = None
