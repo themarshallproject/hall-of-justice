373f8bdecb4ce73ef04aac0b251d744efd962c4f
Albert Bowden <albert@sunlightfoundation.com>
2016-02-01 11:12:56 -0500

    my commit message

--- hallofjustice/settings.py
+++ hallofjustice/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', None)
+SECRET_KEY = os.getenv('SECRET_KEY', "idontknow")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
